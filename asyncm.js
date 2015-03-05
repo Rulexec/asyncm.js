1bcc49af8d25341c36ae31e090092c3b92bb1b96
Alexander Ruliov <alexander.ruliov@gmail.com>
2015-03-05 22:20:38 +0300

    Initial commit

---
+++ asyncm.js
@@ -0,0 +1,391 @@
+(function(global) {
+  var tick, clearTick;
+  if (typeof setImmediate === 'function') {
+    tick = setImmediate;
+    clearTick = clearImmediate;
+  } else {
+    tick = function(f) { return setTimeout(f, 0); };
+    clearTick = clearTimeout;
+  }
+
+  M.tick = function() {
+    var args = arguments;
+    return M.sleep(0).then(function() {
+      return M.pure.apply(null, args);
+    });
+  };
+
+  M.ALREADY_FINISHED = 'already_finished';
+  M.CANCELLED = 'cancelled';
+  M.CANNOT_BE_CANCELLED = 'cannot_be_cancelled';
+
+  M.pure = function() {
+    var args = arguments;
+
+    return M(function(callback) {
+      callback.apply(null, args);
+      return M.alreadyFinished();
+    });
+  };
+  M.pureF = function(f) {
+    return M(function(callback, options) {
+      callback(null, f(options));
+      return M.alreadyFinished();
+    });
+  };
+  M.pureM = function(f) {
+    return M(function(callback, options) {
+      return f(options).run(callback, options);
+    });
+  };
+
+  M.lazy = function(f) {
+    var g = function(callback, options) {
+      var m = f(options);
+
+      g = function(callback, options) {
+        return m.run(callback, options);
+      };
+
+      return g(callback, options);
+    };
+
+    return M(g);
+  };
+  M.lazyF = function(f) {
+    return M.lazy(function(options) { return M.lazyM(f(options)); });
+  };
+  M.lazyM = function(m) {
+    var result = null;
+    var awaiters = [];
+
+    var g = function(callback, options) {
+      g = function(callback) {
+        var awaiter = {
+          cancelled: false,
+          callback: callback
+        };
+
+        awaiters.push(awaiter);
+
+        return {
+          cancel: function() {
+            awaiter.cancelled = true;
+            return M.pure(null, M.CANCELLED);
+          }
+        };
+      };
+
+      m.run(function() {
+        result = arguments;
+
+        g = function(callback) {
+          callback.apply(null, result);
+          return M.alreadyFinished();
+        };
+
+        g(callback);
+        awaiters.forEach(function(awaiter) {
+          if (awaiter.cancelled) return;
+          g(awaiter.callback);
+        });
+        awaiters = null;
+      }, options);
+
+      // TODO: can be cancelled, if nobody else don't want a result
+      return M.cannotBeCancelled();
+    };
+
+    return M(function() { return g.apply(null, arguments); });
+  };
+
+  M.sleep = function(t) {
+    var setSleep, clearSleep;
+    if (t > 0) {
+      setSleep = function(f) { return setTimeout(f, t); };
+      clearSleep = clearTimeout;
+    } else if (t === 0) {
+      setSleep = tick;
+      clearSleep = clearTick;
+    } else {
+      return M.pure('M.sleep timeout lesser than 0: ' + t);
+    }
+
+    return M(function (callback, options) {
+      var runned = false, cancelled = false;
+
+      var id = setSleep(function() {
+        runned = true;
+
+        callback(null, options);
+      });
+
+      return {
+        cancel: function() { return M(function (callback) {
+          if (runned) { callback(null, M.ALREADY_FINISHED); return; }
+          if (cancelled) { callback(null, M.CANCELLED); return; }
+
+          clearSleep(id);
+          cancelled = true;
+          callback(null, M.CANCELLED);
+
+          return M.alreadyFinished(); // TODO: cancel of cancel is implementable
+        }); }
+      };
+    });
+  };
+
+  M.timeout = function(f, t) {
+    return M.sleep(t).bind(f);
+  };
+
+  M.parallel = function(ps, options) {
+    if (ps.length === 0) return M.pure(null, []);
+
+    return M(function(callback, mOptions) {
+      options = options || {};
+      var errorCatcher = options.errorCatcher,
+          drop = options.drop,
+          single = options.single,
+          limit = Math.min(options.limit || ps.length, ps.length),
+          f = options.f;
+
+      if (typeof errorCatcher !== 'function') errorCatcher = function(){};
+
+      var handleError = function(error) {
+        callback(error);
+
+        handleError = errorCatcher;
+      };
+      
+      var left = ps.length;
+
+      var i;
+      if (!drop) {
+        var results = [];
+        for (i = 0; i < left; i++) results.push(null);
+      }
+
+      var runPos = 0,
+          running = 0;
+
+      for (i = 0; i < limit; i++) {
+        runOne();
+      }
+
+      function runOne() {
+        if (runPos >= ps.length) return;
+
+        var index = runPos;
+        var m = ps[runPos++];
+        if (f) m = f(m, index);
+
+        running++;
+        m.run(function(error) {
+          if (error) {
+            handleError(error);
+            return;
+          }
+
+          if (!drop) {
+            results[index] = Array.prototype.slice.call(arguments, 1);
+            if (single) results[index] = results[index][0];
+          }
+
+          left--;
+
+          if (left === 0) {
+            if (drop) callback(null);
+            else callback(null, results);
+          } else {
+            running--;
+            if (running < limit) {
+              // FIXME
+              setTimeout(runOne, 0);
+            }
+          }
+        }, mOptions);
+      }
+
+      return M.cannotBeCancelled(); // TODO: can be
+    });
+  };
+  
+  M.alreadyFinished = function() {
+    return {
+      cancel: function() { return M.pure(null, M.ALREADY_FINISHED); }
+    };
+  };
+  M.cannotBeCancelled = function() {
+    return {
+      cancel: function() { return M.pure(null, M.CANNOT_BE_CANCELLED); }
+    };
+  };
+
+  function M(run) {
+    if (!(this instanceof M)) return new M(run);
+
+    var self = this;
+
+    this.then = function(f) {
+      return new M(function(callback, options) {
+        var nextRunning = null,
+            cancelled = false,
+            cancelling = false,
+            nextCancelled = false;
+
+        var running = self.run(function() {
+          if (!cancelling) {
+            var args = arguments;
+
+            nextRunning = (function() {
+              var m;
+              try {
+                m = f.apply(null, args);
+              } catch (e) {
+                return M.pure(e);
+              }
+
+              if (!(m instanceof M)) {
+                if (m === undefined) {
+                  m = M.pure.bind(null, null).apply(null, args);
+                } else {
+                  m = M.pure(null, m);
+                }
+              }
+              /*if (!(m instanceof M)) {
+                //
+                // jshint debug:true
+                debugger;
+                return M.pure(new Error('f not returns a M instance!'));
+              }*/
+
+              return m;
+            })().run(callback, options);
+          } else {
+            nextCancelled = true;
+          }
+        }, options);
+
+        var alreadyFinished = false,
+            cannotBeCancelled = false;
+
+        return {
+          cancel: function() {
+            if (cancelled) return M.pure(null, M.CANCELLED);
+            if (alreadyFinished) return M.pure(null, M.ALREADY_FINISHED);
+            if (cannotBeCancelled) return M.pure(null, M.CANNOT_BE_CANCELLED);
+
+            cancelling = true;
+
+            return running.cancel().bind(function(status) {
+              if (status === M.CANCELLED) {
+                cancelled = true;
+                return M.pure(null, M.CANCELLED);
+              } else if (status === M.ALREADY_FINISHED) {
+                if (nextCancelled) {
+                  cancelled = true;
+                  return M.pure(null, M.CANCELLED);
+                } else {
+                  return nextRunning.cancel().bind(function(nextStatus) {
+                    if (nextCancelled) {
+                      cancelled = true;
+                      return M.pure(null, M.CANCELLED);
+                    }
+
+                    switch (nextStatus) {
+                    case M.CANCELLED:
+                      cancelled = true;
+                      return M.pure(null, M.CANCELLED);
+                    case M.ALREADY_FINISHED:
+                      alreadyFinished = true;
+                      return M.pure(null, M.ALREADY_FINISHED);
+                    case M.CANNOT_BE_CANCELLED:
+                      cannotBeCancelled = true;
+                      return M.pure(null, M.CANNOT_BE_CANCELLED);
+                    default: throw new Error('Unknown status: ' + nextStatus);
+                    }
+                  });
+                }
+              } else if (status === M.CANNOT_BE_CANCELLED) {
+                cannotBeCancelled = true;
+                return M.pure(null, M.CANNOT_BE_CANCELLED);
+              } else {
+                throw new Error('Unknown status: ' + status);
+              }
+            });
+          }
+        };
+      });
+    };
+
+    this.bind = function(f) {
+      return self.then(function(error) {
+        if (error) return M.pure(error);
+        else return f.apply(null, Array.prototype.slice.call(arguments, 1));
+      });
+    };
+
+    this.bindError = function(f) {
+      return self.then(function(error) {
+        if (error) return f(error);
+        else return M.pure.apply(null, arguments);
+      });
+    };
+
+    this.skip = function(m) {
+      return self.bind(function() { return m; });
+    };
+
+    this.see = function(f) {
+      return self.then(function() {
+        f.apply(null, arguments);
+        return M.pure.apply(null, arguments);
+      });
+    };
+
+    this.seeBind = function(f) {
+      return self.bind(function() {
+        f.apply(null, arguments);
+        return M.pure.bind(null, null).apply(null, arguments);
+      });
+    };
+
+    this.fmap = function(f) {
+      return self.bind(function() {
+        return M.pure(null, f.apply(null, arguments));
+      });
+    };
+
+    this.run = function(callback, options) {
+      if (typeof callback !== 'function') callback = function(){};
+      if (!options) options = {};
+
+      return normalizeRunning(run(callback, options));
+    };
+
+    function normalizeRunning(running) {
+      if (!running) running = {};
+      if (!running.cancel) running.cancel = function() {
+        return M(function(callback) {
+          callback(null, M.CANNOT_BE_CANCELLED);
+          return M.cannotBeCancelled();
+        });
+      };
+      return running;
+    }
+  }
+
+  if (typeof module !== 'undefined' && module.exports !== 'undefined') {
+    module.exports = M;
+  } else if (typeof angular !== undefined && typeof angular.module === 'function') {
+    angular.module('asyncm', []).service('M', function() { return M; });
+  } else {
+    var oldM = window.M;
+    global.M = M;
+    global.M.noConflict = function() {
+      if (global.M === M) global.M = oldM;
+      return M;
+    };
+  }
+})(this);
---
+++ test/basicTest.js
@@ -0,0 +1,106 @@
+var assert = require('assert'),
+
+    M = require('../asyncm');
+
+describe('M', function() {
+  describe('.pure', function() {
+    it('binds on non-error', function(done) {
+      M.pure(null, 'answer', 42).bind(function(text, number) {
+        return M.pure(null, 'The ' + text[0].toUpperCase() + text.slice(1) + ' is ' + number);
+      }).run(function(error, s) {
+        assert.ifError(error);
+        assert.equal(s, 'The Answer is 42');
+        setImmediate(done);
+      });
+    });
+
+    it('then', function(done) {
+      M.pure('scary', 13).then(function(scary, thirteen) {
+        return M.pure(null, scary + ' ' + thirteen);
+      }).run(function(error, s) {
+        assert.ifError(error);
+        assert.equal(s, 'scary 13');
+        setImmediate(done);
+      });
+    });
+
+    it('bindError not calls on non-error', function(done) {
+      M.pure(null, 'no error').bindError(function() {
+        expect('').toBe('Failed');
+      }).run(function(error, s) {
+        assert(!error, error);
+        assert.equal(s, 'no error');
+        setImmediate(done);
+      });
+    });
+
+    it('bindError', function(done) {
+      M.pure('some-error').bindError(function(error) {
+        assert.equal(error, 'some-error');
+        return M.pure('other-error');
+      }).bind(function() {
+        assert(false, 'Should not be runned');
+      }).bindError(function(error) {
+        assert.equal(error, 'other-error');
+        return M.pure(null, 'no error');
+      }).run(function(error, s) {
+        assert.ifError(error);
+        assert.equal(s, 'no error');
+        setImmediate(done);
+      });
+    });
+
+    it('bind passes data further, if function is not returned M instance', function(done) {
+      M.pure(null, 1, 2, 3).bind(function(a, b, c) {
+        return M.pure(null, a + b + c);
+      }).bind(function(x) {
+        assert.equal(x, 6);
+      }).run(function(error, x) {
+        assert.ifError(error);
+        setImmediate(done);
+      });
+    });
+
+    it('bind works like fmap, if function is returned not an M instance and not undefined', function(done) {
+      M.pure(null, 1, 2, 3).bind(function(a, b, c) {
+        return a + b + c;
+      }).run(function(error, x) {
+        assert.ifError(error);
+        assert.equal(x, 6);
+        setImmediate(done);
+      });
+    });
+  });
+
+  describe('.timeout accepts function, that returns async and timeout in ms', function() {
+    var TIMEOUT = 30;
+
+    it('runs function after this timeout and runs returned async', function(done) {
+      var startTime = new Date().getTime();
+
+      M.timeout(function() {
+        return M.pure(null, new Date().getTime());
+      }, 100).run(function(error, time) {
+        assert.ifError(error);
+
+        assert(time - startTime >= TIMEOUT);
+        done();
+      });
+    });
+  });
+
+  describe('.parallel accepts array of asyncs', function() {
+    it('forms async, that returns array of arrays of success results', function(done) {
+      M.parallel([
+        M.pure(null, 42),
+        M.timeout(function() { return M.pure(null, 13); }, 5)
+      ]).run(function(error, results) {
+        assert.ifError(error);
+
+        assert.deepEqual(results, [[42], [13]]);
+        
+        done();
+      });
+    });
+  });
+});
